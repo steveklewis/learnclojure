0d0c1d00afd77b2004e1f800fda631da7b8e5d4b
steveklewis <lewisksteven@gmail.com>
2015-02-11 22:02:53 -0500

    Reading JSON

--- surfchase/project.clj
+++ surfchase/project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
 		 [org.clojure/data.json "0.2.5"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+		 [org.clojure/data.json "0.2.5"]]
   :main ^:skip-aot surfchase.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- surfchase/src/surfchase/core.clj
+++ surfchase/src/surfchase/core.clj
@@ -1,8 +1,8 @@
 (ns surfchase.core
+  (:require [clojure.data.json :as json])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-
-  (println (slurp (.getFile (clojure.java.io/resource "beaches.json"))))
+  (print (json/read-str (slurp (.getFile (clojure.java.io/resource "beaches.json"))))))
