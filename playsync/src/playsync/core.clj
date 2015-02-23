ecb4f045ebdf1b5df26bf7c7cb0b87b45ad25deb
steveklewis <lewisksteven@gmail.com>
2015-02-22 20:46:36 -0500

    Document what I'm doing in main.

--- playsync/src/playsync/core.clj
+++ playsync/src/playsync/core.clj
@@ -4,8 +4,13 @@
              :refer [>! <! >!! <!! go chan buffer close! thread
                      alts! alts!! timeout]]))
 
+
+
 (defn -main
-  "I don't do a whole lot."
+  "1. Create a channel.
+   2. Create a go block which uses a parking take to 
+   print what comes over the channel.
+   3. Use a blocking put to push ketchup onto the channel."
   [& args]
   (def echo-chan (chan))
   (go (println (<! echo-chan)))
