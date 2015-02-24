76f73a3122a9d550343de0e64905d2cb2b8a17e7
steveklewis <lewisksteven@gmail.com>
2015-02-23 22:20:08 -0500

    Added log4j

--- logging/project.clj
+++ logging/project.clj
@@ -3,6 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jdmk/jmxtools
+                                                    com.sun.jmx/jmxri]]]
   :main logging.core
 )
--- logging/src/logging/core.clj
+++ logging/src/logging/core.clj
@@ -1,4 +1,6 @@
-(ns logging.core)
+(ns logging.core
+  (:require [clojure.tools.logging :as log]))
 
 (defn -main [& args]
+  (log/info "Starting the app")
   (println "Hello, World!"))
