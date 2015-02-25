21450b84bd02d6d39dd3d1b94d1ba65f42d30aa1
steveklewis <lewisksteven@gmail.com>
2015-02-24 20:29:08 -0500

    Logging to a file using environment variable to switch environments.

--- logging/src/logging/core.clj
+++ logging/src/logging/core.clj
@@ -2,5 +2,6 @@
   (:require [clojure.tools.logging :as log]))
 
 (defn -main [& args]
-  (log/info "Starting the app")
-  (println "Hello, World!"))
+  (let [env (get (System/getenv) "CLJ_ENV" "development")]
+    (log/info (format "Starting the app with %s environment" env)
+    (println "Hello, World!"))))
