d405fa63d2bbebedd9ae16949a3bad18be4e2edc
steveklewis <lewisksteven@gmail.com>
2015-02-26 22:12:25 -0500

    Getting more familiar with core.async

--- playsync/src/playsync/core.clj
+++ playsync/src/playsync/core.clj
@@ -2,16 +2,22 @@
   (:require [clojure.core.async
              :as a
              :refer [>! <! >!! <!! go chan buffer close! thread
-                     alts! alts!! timeout]]))
+                     alts! alts!! timeout go-loop]]))
 
 
 
 (defn -main
   "1. Create a channel.
    2. Create a go block which uses a parking take to 
-   print what comes over the channel.
+   print what comes over the channel. Modified to add
+   the infinite loop to print anything that comes over
+   the channel.
    3. Use a blocking put to push ketchup onto the channel."
   [& args]
   (def echo-chan (chan))
-  (go (println (<! echo-chan)))
-  (>!! echo-chan "ketchup"))
+  (go (while true (println (<! echo-chan))))
+  (go (>! echo-chan "ketchup"))
+  (println "Trying to print mayo")
+  (go (>! echo-chan "mayonnaise"))
+  (Thread/sleep 10000))
+  
