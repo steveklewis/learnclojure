72d862cfb3c7a29dfe3524abe3f5dfaf6166b987
steveklewis <lewisksteven@gmail.com>
2015-02-26 22:28:28 -0500

    Looping over events.

--- surfchase/project.clj
+++ surfchase/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-		 [org.clojure/data.json "0.2.5"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+		 [org.clojure/data.json "0.2.5"]]
   :main ^:skip-aot surfchase.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
