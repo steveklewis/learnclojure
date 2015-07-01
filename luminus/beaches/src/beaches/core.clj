76de57d46c1b37362543f398a56fa91b013cbe27
steveklewis <lewisksteven@gmail.com>
2015-06-30 22:54:57 -0400

    Initial commit of luminus-beaches

---
+++ luminus/beaches/src/beaches/core.clj
@@ -0,0 +1,35 @@
+(ns beaches.core
+  (:require [beaches.handler :refer [app init destroy]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.reload :as reload]
+            [taoensso.timbre :as timbre]
+            [environ.core :refer [env]])
+  (:gen-class))
+
+(defonce server (atom nil))
+
+(defn parse-port [[port]]
+  (Integer/parseInt (or port (env :port) "3000")))
+
+(defn start-server [port]
+  (init)
+  (reset! server
+          (run-jetty
+            (if (env :dev) (reload/wrap-reload #'app) app)
+            {:port port
+             :join? false})))
+
+(defn stop-server []
+  (when @server
+    (destroy)
+    (.stop @server)
+    (reset! server nil)))
+
+(defn start-app [args]
+  (let [port (parse-port args)]
+    (.addShutdownHook (Runtime/getRuntime) (Thread. stop-server))
+    (timbre/info "server is starting on port " port)
+    (start-server port)))
+
+(defn -main [& args]
+  (start-app args))
---
+++ luminus/beaches/test/beaches/test/handler.clj
@@ -0,0 +1,13 @@
+(ns beaches.test.handler
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :refer :all]
+            [beaches.handler :refer :all]))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= 200 (:status response)))))
+
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= 404 (:status response))))))
