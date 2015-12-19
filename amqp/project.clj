3a910f4fb40360c50690c8f0e959a9676aaee864
steveklewis <lewisksteven@gmail.com>
2015-12-18 21:08:10 -0500

    First small test with AMQP in Clojure

---
+++ amqp/project.clj
@@ -0,0 +1,10 @@
+(defproject amqp "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.novemberain/langohr "3.3.0"]]
+  :main ^:skip-aot amqp.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
