06394f33b8b8af964031c38aae956b92aee8769c
steveklewis <lewisksteven@gmail.com>
2016-06-18 10:45:57 -0400

    First Ring hello world app

---
+++ ring/hello-world/project.clj
@@ -0,0 +1,11 @@
+(defproject hello-world "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
+  :main ^:skip-aot hello-world.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
